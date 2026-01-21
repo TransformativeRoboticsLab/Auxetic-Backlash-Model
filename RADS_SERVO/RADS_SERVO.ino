9dc0a92b7355492e6e5708059448a610497f10c3
aRandomHumanoid <51519362+aRandomHumanoid@users.noreply.github.com>
2026-01-21 18:01:45 -0500

    boop

--- RADS_SERVO/RADS_SERVO.ino
+++ RADS_SERVO/RADS_SERVO.ino
@@ -1,14 +1,31 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <ServoInput.h>
 const int X = 3; // X axis
 const int Y = 3; // Y axis
-const int SERVONUM = 2; // Z axis
-int state[SERVONUM][X][Y]
+const int SERVONUM = 2; // linear or expanding servo; 0 is linear, 1 is expanding
+
+ServoInputPin<1> encoder0;
+ServoInputPin<2> encoder2;
+ServoInputPin<3> encoder3;
+ServoInputPin<4> encoder4;
+ServoInputPin<5> encoder5;
+ServoInputPin<6> encoder6;
+ServoInputPin<7> encoder7;
+ServoInputPin<8> encoder8;
+
+ServoInputPinBase* encoders[] = {&encoder0, &encoder1, &encoder2, &encoder3, &encoder4, &encoder5, &encoder6 , &encoder7, &encoder8}
+
+int targetStates[SERVONUM][X][Y];
+int linCurrentStates[X][Y]; //keeps track of the current states of the linear actuator
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  150 // Minimum pulse length count (out of 4096)
-#define SERVOMAX  600 // Maximum pulse length count (out of 4096)
+#define SERVOMIN  205 // Minimum pulse length count (out of 4096)
+#define SERVOMAX  410 // Maximum pulse length count (out of 4096)
+#define LINSERVOMIN 200 //Min pulse len (conservative)
+#define LINSERVOMID 288 //point of linear servo middle
+#define LINSERVOMAX 350 // Max pulse len (conservative)
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
 #define NUM_SERVOS 16 // Total number of servo channels
 
@@ -37,6 +54,8 @@ void setup() {
   Serial.begin(9600);
   Serial.println("16 channel simultaneous servo test!");
 
+  for (auto* s : inputs) s->attach();
+
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
   pwm.setPWMFreq(SERVO_FREQ);
@@ -50,7 +69,10 @@ void loop() {
     String line = Serial.readStringUntil('\n');
     parseState(line);
   }
-  updateServoPositions();
+  //updateServoPositions();
+  pwm.setPWM(1, 0, 288); //292 stops, 284 stops; call it 288
+/*
+
   // Move all 16 servos from 0° to 180° simultaneously
   Serial.println("Moving all servos to 180 degrees");
   for (uint16_t pulselen = SERVOMIN; pulselen <= SERVOMAX; pulselen++) {
@@ -72,26 +94,39 @@ void loop() {
   }
 
   delay(1000); // Pause at 0°
+*/
 }
 
 void updateServoPositions(){
-  int servoIndex = 0
+  int servoIndex = 0;
   //updating linear servos
   for (int x = 0; x < X; x++) {
       for (int y = 0; y < Y; y++) {
-        
-        pwm.setPwm(servoIndex, state[servo][x][y])
+        int currentState = linCurrentStates[x][y];
+        int targetState = targetStates[0][x][y];
+        int delta = targetState - currentState;
+        linearMove(servoIndex, delta);
+        linCurrentStates[x][y] = targetState;
+        servoIndex++;
       }
     }
   //updating expanding servos
   for (int x = 0; x < X; x++) {
       for (int y = 0; y < Y; y++) {
-        pwm.setPwm(servoIndex, state[servo][x][y])
+        rotateMove(servoIndex, targetStates[1][x][y]);
+        servoIndex++;
       }
     }
 }
 
-void setServo(int servoIndex, int )
+void linearMove(int servoNum, int delta) {
+
+}
+
+void rotateMove(int servoNum, int angle) {
+  int pulseLen = map(angle, 0, 180, SERVOMIN, SERVOMAX);
+  pwm.setPWM(servoNum, 0, pulseLen);
+}
 
 void parseState(String line) {
   line.trim();
@@ -124,9 +159,9 @@ void parseState(String line) {
   // Rebuild 3D array
   int k = 0;
   for (int z = 0; z < SERVONUM; z++) {
-    for (int x = 0; x < X; X++) {
-      for (int y = 0; y < Y; Y++) {
-        state[z][x][y] = values[k++];
+    for (int x = 0; x < X; x++) {
+      for (int y = 0; y < Y; y++) {
+        targetStates[z][x][y] = values[k++];
       }
     }
   }
